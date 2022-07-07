1294eace1641588881ecd0ffd2f9fd33b5184405
gordo <rogelioalbertopedraza@gmail.com>
2022-07-07 11:09:05 -0500

    thought routes complete

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+
+const {
+  findAllThoughts,
+  findThoughtById,
+  createAThought,
+  updateAThought,
+  addAReaction,
+  deleteAThought,
+  deleteAReaction,
+} = require("../../controllers/thought-controller");
+
+router.route("/").get(findAllThoughts);
+
+router
+  .route("/:id")
+  .get(findThoughtById)
+  .put(updateAThought)
+  .delete(deleteAThought);
+
+router.route("/:userId").post(createAThought);
+
+router.route("/:thoughtId/reactions").post(addAReaction);
+
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteAReaction);
+
+module.exports = router;
